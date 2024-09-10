1b7ab604c7df5840b13a99b20afdba9a0c0b8582
Laytong Ly <127829107+laytonglee@users.noreply.github.com>
2024-09-10 14:31:12 +0700

    Update index.blade.php

--- LoginAndRegister/resources/views/reports/acc-status-his/index.blade.php
+++ LoginAndRegister/resources/views/reports/acc-status-his/index.blade.php
@@ -1,1 +1,208 @@
+<x-testcomp>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Account Status History</h4>
+                    </div>
+
+                    <div class="search-container">
+                        <div class="search-panel">
+
+
+                            <form action="{{ url('customers/all-customers') }}" method="GET" id="customerSearchForm">
+                                <table class="search-table">
+                                    <tr>
+                                        <th colspan="18" style="text-align: center;">Search Status Change History</th>
+                                    </tr>
+                                    <tr>
+
+                                        <td><label for="create-date">Operation Date: </label></td>
+                                        <td>
+                                            <input type="text" class="form-control" id="create-date" name="create-date" value="">
+                                        </td>
+                                        
+                                        
+                                        <td><label for="">to</label></td>
+                                        <td>
+                                            <input type="text" class="form-control" id="create-date-to" name="create-date-to" value="">
+                                        </td>
+                                        <td colspan="13">
+                                            <button type="button" class="btn btn-secondary" id="">Search</button>
+                                            <button type="button" class="btn btn-secondary" id="">Reset</button>
+                                            <button type="button" class="btn btn-secondary" id="">Print</button>
+                                        </td>
+                                        
+                                    </tr>
+                                </table>
+                            </form>
+
+
+                        </div>
+                    </div>
+
+
+                    
+                    <div class="container">
+                        <div class="card-header">
+                        <h4>Account Status History</h4>
+                        <h4>Account Status History</h4>
+                        <h4>Account Status History</h4>
+                        <h4>Account Status History</h4>
+                        <h4>Account Status History</h4>
+                        <h4>Account Status History</h4>
+                        <h4>Account Status History</h4>
+                        <h4>Account Status History</h4>
+                        <h4>Account Status History</h4>
+                        <h4>Account Status History</h4>
+                        <h4>Account Status History</h4>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="card-header" style="display: flex; gap: 5px;">
+                            <table class="table table-bordered table-striped" style="flex: 1;">
+                                <thead style="background-color: #7A7A7A; color:white">
+                                    <tr>
+                                        <th style="text-align: center; vertical-align: middle;" colspan="9">Status Changes per Week</th>
+
+                                    </tr>
+                                    <tr>
+                                        <th style="text-align: center; vertical-align: middle;">Duration</th>
+                                        <th style="background-color: #418CF0"></th>
+                                        <th style="background-color: #FCB441"></th>
+                                        <th style="background-color: #E0400A"></th>
+                                        <th style="background-color: #BFBFBF"></th>
+                                        <th style="background-color: #1A3B69"></th>
+                                        <th style="background-color: #FFE382"></th>
+                                        <th style="background-color: #129CDD"></th>
+                                        <th style="text-align: center; vertical-align: middle;">Total</th>
+                                    </tr>
+                                </thead>
+    
+                                <tbody>
+                                    <tr>
+                                        <td>09/09/2023 - 09/09/2023</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                    </tr>
+                                    <tr>
+                                        <td>09/09/2023 - 09/09/2023</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                    </tr>
+                                    <tr>
+                                        <td>09/09/2023 - 09/09/2023</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                    </tr>
+                                    <tr>
+                                        <td>09/09/2023 - 09/09/2023</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                    </tr>
+                                    <tr>
+                                        <td>09/09/2023 - 09/09/2023</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                        <td>0</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <table class="table table-bordered table-striped" style="flex: 1;">
+                                <thead style="background-color: #7A7A7A; color:white">
+                                    <tr>
+                                        <th style="text-align: center; vertical-align: middle;" colspan="6">Account Status History (242)</th>
+
+                                    </tr>
+                                    <tr>
+                                        <th style="text-align: center; vertical-align: middle;">Date</th>
+                                        <th style="text-align: center; vertical-align: middle;">Account ID</th>
+                                        <th style="text-align: center; vertical-align: middle;">Old Status</th>
+                                        <th style="text-align: center; vertical-align: middle;">New Status</th>
+                                        <th style="text-align: center; vertical-align: middle;">Comment</th>
+                                        <th style="text-align: center; vertical-align: middle;">Invoice ID</th>
+
+
+                                    </tr>
+                                </thead>
+    
+                                <tbody>
+                                    <tr>
+                                        <td>11/09/2023 15:07:00</td>
+                                        <td>FO202300084</td>
+                                        <td>Inactive</td>
+                                        <td>Active</td>
+                                        <td>Issue inv</td>
+                                        <td>TI23-00042</td>
+                                    </tr>
+                                    <tr>
+                                        <td>11/09/2023 15:07:00</td>
+                                        <td>FO202300084</td>
+                                        <td>Inactive</td>
+                                        <td>Active</td>
+                                        <td>Issue inv</td>
+                                        <td>TI23-00042</td>
+                                    </tr>
+                                    <tr>
+                                        <td>11/09/2023 15:07:00</td>
+                                        <td>FO202300084</td>
+                                        <td>Inactive</td>
+                                        <td>Active</td>
+                                        <td>Issue inv</td>
+                                        <td>TI23-00042</td>
+                                    </tr>
+                                    <tr>
+                                        <td>11/09/2023 15:07:00</td>
+                                        <td>FO202300084</td>
+                                        <td>Inactive</td>
+                                        <td>Active</td>
+                                        <td>Issue inv</td>
+                                        <td>TI23-00042</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>  
+    </div>
+        
+</x-testcomp>
+
 
