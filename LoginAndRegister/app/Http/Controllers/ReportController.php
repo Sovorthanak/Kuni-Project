cfd3ba893bf134786ffcb1098a91b97c8c512f36
Laytong Ly <127829107+laytonglee@users.noreply.github.com>
2024-09-10 14:34:16 +0700

    Update ReportController.php

--- LoginAndRegister/app/Http/Controllers/ReportController.php
+++ LoginAndRegister/app/Http/Controllers/ReportController.php
@@ -1,13 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Customer;
 
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function index(){
+        $customers = Customer::all(); // Assuming you have a Customer model
 
-        return view('reports.acc-report.index');
+        return view('reports.acc-report.index', compact('customers'));
+    }
+
+    public function accStatusHis(){
+
+        return view('reports.acc-status-his.index');
     }
 }
